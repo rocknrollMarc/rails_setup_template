2f924233204a81fc2a8259cb34dfda18cf5f354b
aeonscope <aeonscope@gmail.com>
2009-10-20 18:15:01 -0600

    Added more gems and cleaned up the template code.

---
+++ rails/config/environments/development.rb
@@ -0,0 +1,17 @@
+
+# Bullet Settings
+config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  begin
+    require "ruby-growl"
+    Bullet.growl = true
+    Bullet.growl_password = "GXzsP2dh)Q93H.fE!Rux"
+  rescue MissingSourceFile
+    STDERR.puts "Unable to enable Growl support: #{$!}"
+  end
+  Bullet.rails_logger = true
+  Bullet.disable_browser_cache = true
+end
