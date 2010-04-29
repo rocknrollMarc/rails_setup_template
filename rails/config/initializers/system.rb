8a7b3af7cdac8382803682f4ae30d490d58d02f4
aeonscope <aeonscope@gmail.com>
2010-04-28 19:58:01 -0600

    Changed default developer link.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -4,8 +4,8 @@ SYSTEM_DESCRIPTION = "A demo application."
 SYSTEM_VERSION = "0.1.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
 
-DEVELOPER_LABEL = "Berserk Technologies"
-DEVELOPER_URL = "http://www.berserktech.com"
+DEVELOPER_LABEL = "Aeonscope"
+DEVELOPER_URL = "http://www.aeonscope.net"
 
 THEME_NAME = "default"
 THEME_ROOT = "/themes/#{THEME_NAME}"
