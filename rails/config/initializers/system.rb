1919b4ad5d9670b6af1965d8993bcde8a4584d9c
aeonscope <aeonscope@gmail.com>
2010-07-18 16:01:04 -0600

    Updated profile info.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -4,8 +4,8 @@ SYSTEM_DESCRIPTION = "A demo application."
 SYSTEM_VERSION = "0.1.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
 
-DEVELOPER_LABEL = "Aeonscope"
-DEVELOPER_URL = "http://www.aeonscope.net"
+DEVELOPER_LABEL = "Brooke Kuhlmann"
+DEVELOPER_URL = "http://www.google.com/profiles/114291115422773881390"
 
 THEME_NAME = "default"
 THEME_ROOT = "/themes/#{THEME_NAME}"
