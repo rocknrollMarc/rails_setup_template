8dfa58ac29ca1e84bf9a8b9bd56e3defc1226abb
aeonscope <aeonscope@gmail.com>
2010-09-19 21:36:12 -0600

    Version 1.2.0.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -1,7 +1,7 @@
 # System-wide settings.
 SYSTEM_LABEL = "Demo"
 SYSTEM_DESCRIPTION = "A demo application."
-SYSTEM_VERSION = "1.1.0"
+SYSTEM_VERSION = "1.2.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
 
 DEVELOPER_LABEL = "Brooke Kuhlmann"
