5bba70477d212135492a07851004ab53059df480
brooke <brooke@redalchemist.com>
2011-06-07 20:26:22 -0600

    Removed the Bullet gem. Proving to be too buggy.

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -7,18 +7,3 @@
     Debugger.start_remote
     File.delete File.join(Rails.root, "tmp", "debug.txt")
   end
-
-  # Bullet Settings
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.console = true
-    Bullet.bullet_logger = true
-    Bullet.rails_logger = true
-    Bullet.disable_browser_cache = true
-    begin
-      require "ruby-growl"
-      Bullet.growl = {:password => "enter password"}
-    rescue MissingSourceFile
-      STDERR.puts "Unable to enable Growl support: #{$!}"
-    end
-  end
