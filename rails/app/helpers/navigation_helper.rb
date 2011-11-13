0c32ff4ffd3363469e605dc17c48843709c14024
Brooke Kuhlmann <brooke@redalchemist.com>
2011-11-13 11:37:10 -0700

    Removed the current_route method from the navigation helper.

--- rails/app/helpers/navigation_helper.rb
+++ rails/app/helpers/navigation_helper.rb
@@ -1,21 +1,4 @@
 module NavigationHelper
-	# Answer the current route for account navigation within the site. This is based on the type and role of the current
-	# user. Routing is broken down into three sections which loads the correct controller and navigation menu. For example:
-	# * shared/menus/_admin.html.erb
-	# * shared/menus/_member.html.erb
-	# * shared/menus/_visitor.html.erb
-	def current_route
-	  if current_user
-  	  case current_user.type
-      when "Employee" then "admin"
-      when "Customer" then "member"
-      else "visitor"
-      end
-    else
-      "visitor"
-    end
-	end
-	
 	# Render a breadcrumb.
   def breadcrumb text, url = nil
     content_tag :li, (url ? link_to(text, url) : content_tag(:span, text))
