df48ba80c72527836f19dcc727c7b70c8513d56b
Brooke Kuhlmann <brooke@redalchemist.com>
2012-01-15 17:03:59 -0700

    Updated the README and CHANGELOG files.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -1,7 +1,7 @@
 # System-wide settings.
 SYSTEM_LABEL = "Rails Setup Template"
 SYSTEM_DESCRIPTION = "An auto-generated skeleton application."
-SYSTEM_VERSION = "3.0.0"
+SYSTEM_VERSION = "4.0.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
 
 DEVELOPER_LABEL = "Red Alchemist"
