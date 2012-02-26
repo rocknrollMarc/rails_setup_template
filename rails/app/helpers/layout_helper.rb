39e42ef2dcd32b4c3acbac24f46cd87dc5def646
Brooke Kuhlmann <brooke@redalchemist.com>
2012-02-26 09:13:28 -0700

    Removed the title helper so that system name is a suffix and not a prefix.

--- rails/app/helpers/layout_helper.rb
+++ rails/app/helpers/layout_helper.rb
@@ -3,8 +3,8 @@ module LayoutHelper
   # within the page header.  Accepts the following arguments:
   # * *title* - The title of the page, defaults to a titleized version of the action name.
   # * *separator* - The sub-page separator (i.e. My Site - Sub-page), defaults to:  '-'.
-  def title title = action_name.titleize, separator = '-'
-    content_for(:title) {[SYSTEM_LABEL, title].compact * " #{separator} "}
+  def title title = action_name.titleize, separator = '|'
+    content_for(:title) {[title, SYSTEM_LABEL].compact * " #{separator} "}
   end
   
   # Dynamically loads stylesheet files.
