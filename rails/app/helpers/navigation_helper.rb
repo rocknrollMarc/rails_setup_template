e18d4af1588ff3f42f852a606f23f443620fdde1
Brooke Kuhlmann <brooke@redalchemist.com>
2012-02-26 09:17:10 -0700

    Switched the breadcrumbs helper to use a class instead of an ID for better campatibility with Twitter Bootstrap.

--- rails/app/helpers/navigation_helper.rb
+++ rails/app/helpers/navigation_helper.rb
@@ -1,12 +1,11 @@
 module NavigationHelper
-	# Render a breadcrumb.
   def breadcrumb text, url = nil
     content_tag :li, (url ? link_to(text, url) : content_tag(:span, text))
   end
-
-  # Render breadcrumb navigation.
+  
+  # Renders breadcrumb navigation.
   # * +crumbs+ - The breadcrumb array.
   def breadcrumbs *crumbs
-    content_for :breadcrumbs, content_tag(:ul, (raw(crumbs.compact.join("\n"))), id: :breadcrumbs)
+    content_for :breadcrumbs, content_tag(:ul, (raw(crumbs.compact.join("\n"))), class: "breadcrumb")
   end
 end
