d48b58eedbf33620a38eb083cb4e4198dd85aff6
Brooke Kuhlmann <brooke@redalchemist.com>
2012-03-17 07:45:36 -0600

    Removed Deadweight gem support.

--- rails/lib/tasks/deadweight.rake
+++
@@ -1,8 +0,0 @@
-require 'deadweight'
-
-desc "Runs Deadweight. Usage: rake deadweight"
-Deadweight::RakeTask.new do |dw|
-  dw.root = "http://localhost:3000"
-  dw.stylesheets = ["/assets/shared/style.css"]
-  dw.pages = ["/"]
-end
--- template.rb
+++ template.rb
@@ -82,7 +82,6 @@ download_file "#{TEMPLATE_ROOT}/rails/app/views/about/show.html.erb", "app/views
 # Lib
 download_file "#{TEMPLATE_ROOT}/rails/lib/tasks/passenger.rake", "lib/tasks/passenger.rake"
 download_file "#{TEMPLATE_ROOT}/rails/lib/tasks/railroad.rake", "lib/tasks/railroad.rake"
-download_file "#{TEMPLATE_ROOT}/rails/lib/tasks/deadweight.rake", "lib/tasks/deadweight.rake"
 
 # Images
 remove_file "app/assets/images/rails.png"
