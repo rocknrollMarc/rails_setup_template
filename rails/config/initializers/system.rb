7394802351cccc85857774ca5b9f3d28fefd9b95
Brooke Kuhlmann <brooke@redalchemist.com>
2012-07-04 13:00:11 -0600

    Updated version info.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -1,7 +1,7 @@
 # System-wide settings.
 SYSTEM_LABEL = "Rails Setup Template"
 SYSTEM_DESCRIPTION = "An auto-generated skeleton application."
-SYSTEM_VERSION = "4.4.0"
+SYSTEM_VERSION = "4.5.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
 
 DEVELOPER_LABEL = "Red Alchemist"
