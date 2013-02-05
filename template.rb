78c73c67fb21eb7fd84334347ea36cdde2d84fe8
Brooke Kuhlmann <brooke@redalchemist.com>
2013-02-04 19:27:24 -0700

    Added CanCan initializer to template setup.

--- template.rb
+++ template.rb
@@ -65,6 +65,7 @@ remove_file "#{application_delta}"
 # Gems
 download_file "#{TEMPLATE_ROOT}/rails/Gemfile", "Gemfile"
 run "bundle install"
+generate "cancan:ability"
 generate "resourcer:install"
 generate "simple_form:install --bootstrap"
 generate "rspec:install"
