1cbb364576a57ccf0d88ad7f715dd09497f71d9c
Brooke Kuhlmann <brooke@redalchemist.com>
2013-02-04 19:37:52 -0700

    Removed developer label and URL info.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -3,6 +3,3 @@ SYSTEM_LABEL = "Rails Setup Template"
 SYSTEM_DESCRIPTION = "An auto-generated skeleton application."
 SYSTEM_VERSION = "4.5.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
-
-DEVELOPER_LABEL = "Red Alchemist"
-DEVELOPER_URL = "http://www.redalchemist.com"
