86bacc58cd17e30d5500638432c72f0cf0a763bc
Brooke Kuhlmann <brooke@redalchemist.com>
2013-05-22 22:01:45 -0600

    Applied version notes to README and CHANGELOG.

--- rails/config/initializers/system.rb
+++ rails/config/initializers/system.rb
@@ -1,5 +1,5 @@
 # System-wide settings.
 SYSTEM_LABEL = "Rails Setup Template"
 SYSTEM_DESCRIPTION = "An auto-generated skeleton application."
-SYSTEM_VERSION = "5.1.0"
+SYSTEM_VERSION = "5.2.0"
 SYSTEM_EMAIL = "noreply@noreply.com"
