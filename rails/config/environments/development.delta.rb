93163cff1c4f5b7f22bee079b49a8c8e757e2c36
Brooke Kuhlmann <brooke@redalchemist.com>
2013-10-07 20:58:10 -0600

    Enhanced development.rb to use xterm colors for web console.

--- rails/config/environments/development.delta.rb
+++ rails/config/environments/development.delta.rb
@@ -2,6 +2,9 @@
   # Enables web console long-polling.
   config.web_console.timeout = 45.seconds
 
+  # Enables web console xterm colors.
+  config.web_console.style.colors = :xterm
+
   # SASS Debugging
   config.sass.debug_info = true
 
