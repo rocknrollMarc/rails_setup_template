fad59b2e9cb7ea439897410be4388a46b9aab302
Brooke Kuhlmann <brooke@redalchemist.com>
2013-11-12 18:49:52 -0700

    Added HTML, CSS, and JavaScript compression.

--- rails/config/application.delta.rb
+++ rails/config/application.delta.rb
@@ -2,6 +2,9 @@
     # Block or throttle abusive clients.
     config.middleware.use Rack::Attack
 
+    # Compress HTML, CSS, and JavaScript files.
+    config.middleware.use Rack::Deflater
+
     # Conditionally force access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
     config.force_ssl = (ENV["HTTPS_ENABLED"] == "yes")
 
