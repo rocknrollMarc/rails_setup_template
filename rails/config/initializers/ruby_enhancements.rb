7976d8fb5268dc06cd31bb51c6edfb9e684c38a6
Brooke Kuhlmann <brooke@redalchemist.com>
2013-12-29 09:19:53 -0700

    Added Ruby BigDecimal inspection enhancement.

--- rails/config/initializers/ruby_enhancements.rb
+++ rails/config/initializers/ruby_enhancements.rb
@@ -4,3 +4,11 @@ class Array
     self.compact.reject {|item| item.blank?}
   end
 end
+
+class BigDecimal
+  # Better inspection, especially when testing.
+  # Reference: https://gist.github.com/henrik/6280438
+  def inspect
+    format "#<BigDecimal:%x %s>", object_id, to_s('F')
+  end
+end
