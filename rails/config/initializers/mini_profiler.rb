0f6e2ee203fa6f9378ba2e48740b425ce108f560
Brooke Kuhlmann <brooke@redalchemist.com>
2014-03-30 11:24:00 -0600

    Fixed loading of mini profiler to development environment only.

--- rails/config/initializers/mini_profiler.rb
+++ rails/config/initializers/mini_profiler.rb
@@ -1,7 +1,8 @@
 # Mini Profiler Settings
+if Rails.env.development?
+  # Start hidden - display with "OPTION+P".
+  Rack::MiniProfiler.config.start_hidden = true
 
-# Start hidden - display with "OPTION+P".
-Rack::MiniProfiler.config.start_hidden = true
-
-# Auto-inject JavaScript support.
-Rack::MiniProfiler.config.auto_inject = false
+  # Auto-inject JavaScript support.
+  Rack::MiniProfiler.config.auto_inject = false
+end
