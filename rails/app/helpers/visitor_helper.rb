bb3493572a529f00202984ba09bf07603f342baa
Brooke Kuhlmann <brooke@redalchemist.com>
2014-07-06 11:19:21 -0600

    Removed the visitor helper.

--- rails/app/helpers/visitor_helper.rb
+++
@@ -1,9 +0,0 @@
-module VisitorHelper
-  # Render the visitor navigation menu.
-  def render_visitor_menu
-    Navigation::Menu.new controller, self, class: "nav" do |visitor|
-      visitor.add "Home", root_path, self
-      visitor.add "About", about_path, self
-    end.render
-  end
-end
