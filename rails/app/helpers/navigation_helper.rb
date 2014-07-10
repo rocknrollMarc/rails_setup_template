632fe756d614a062e3008b0f2bfbe10065aa6925
Brooke Kuhlmann <brooke@redalchemist.com>
2014-07-09 21:17:21 -0600

    Updated navigation helpers and application/admin layouts with Navigator enhancements.

--- rails/app/helpers/navigation_helper.rb
+++ rails/app/helpers/navigation_helper.rb
@@ -9,16 +9,30 @@ module NavigationHelper
     content_for :breadcrumbs, content_tag(:ul, (raw(crumbs.compact.join("\n"))), class: "breadcrumb")
   end
 
-  def render_visitor_menu_left
-    navigation "ul", class: "left" do
-      item "Home", root_path
-      item "About", about_path
-    end
-  end
+  def render_visitor_menu
+    navigation "nav", class: "top-bar", "data-topbar" => nil do
+      ul nil, class: "title-area" do
+        li nil, class: "name" do
+          h1 do
+            a SYSTEM_LABEL, href: root_path
+          end
+        end
+      end
 
-  def render_visitor_menu_right
-    navigation "ul", class: "right" do
-      item "Login", login_path, {}, {class: "button tiny round"}
+      section nil, class: "top-bar-section" do
+        ul nil, class: "left" do
+          item "Home", root_path
+          item "About", about_path
+        end
+
+        ul nil, class: "right" do
+          item render_system_version, '#'
+        end
+
+        ul nil, class: "right" do
+          item "Login", login_path, {}, {class: "button tiny round"}
+        end
+      end
     end
   end
 end
